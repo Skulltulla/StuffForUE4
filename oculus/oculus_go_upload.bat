a6e9e81e3bfc3d25034c69abd3f9463f88ec0116
Michael Peacock <sc4ler@gmail.com>
2019-09-25 14:04:39 -0500

    Added PAUSE to oculus upload script

--- oculus/oculus_go_upload.bat
+++ oculus/oculus_go_upload.bat
@@ -2,3 +2,4 @@ rem This command was used to upload a build for an Oculus Go project to the Ocul
 rem Add ovr-platform-util.exe to the PATH environment variable
 rem Replace: APP_ID, APP_SECRET, FILENAME.apk, FILENAME.obb
 ovr-platform-util.exe upload-mobile-build -a APP_ID -s APP_SECRET --apk FILENAME.apk -c alpha -n "change log notes" --obb FILENAME.obb
+PAUSE
