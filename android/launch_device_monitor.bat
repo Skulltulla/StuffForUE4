bfd17f488aaa6186df751e68e2a99bbf46dfd62e
Michael Peacock <sc4ler@gmail.com>
2019-10-31 13:06:49 -0500

    added android device monitor launch

---
+++ android/launch_device_monitor.bat
@@ -0,0 +1,10 @@
+@echo off
+
+echo. The device monitor is deprecated
+echo.
+echo. https://developer.android.com/studio/profile/monitor
+echo.
+echo To launch:
+PAUSE
+
+%ANDROID_HOME%\tools\monitor.bat
