f7220fb3fdcdf42557f88db85c9987ca41ce9c41
Michael Peacock <sc4ler@gmail.com>
2019-10-31 15:22:31 -0500

    updated oculus go upload to use variables

--- oculus/oculus_go_upload.bat
+++ oculus/oculus_go_upload.bat
@@ -1,5 +1,14 @@
 rem This command was used to upload a build for an Oculus Go project to the Oculus Dashboard
+rem
 rem Add ovr-platform-util.exe to the PATH environment variable
-rem Replace: APP_ID, APP_SECRET, FILENAME.apk, FILENAME.obb
-ovr-platform-util.exe upload-mobile-build -a APP_ID -s APP_SECRET --apk FILENAME.apk -c alpha -n "change log notes" --obb FILENAME.obb
+
+set APP_ID=1234
+set APP_SECRET=1234
+set CHANNEL=alpha
+set NOTES="change notes"
+set APK=ANDROID_ASTC\filename.apk
+set OBB=ANDROID_ASTC\filename.obb
+
+ovr-platform-util.exe upload-mobile-build -a %APP_ID% -s %APP_SECRET% --apk %APK% -c %CHANNEL% -n %NOTES% --obb %OBB%
+
 PAUSE
