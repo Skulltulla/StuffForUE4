8607af36ce9779a073cb8822346f5ddef6e6c25d
Michael Peacock <sc4ler@gmail.com>
2019-11-13 11:42:28 -0600

    Added android wireless connect 'script'

---
+++ android/connect_wireless.bat
@@ -0,0 +1,12 @@
+@echo off
+
+echo Connect to an Android device
+echo wirelessly with these commands:
+echo.
+echo. adb devices
+echo. adb tcpip 5555
+echo. adb shell ip route
+echo. adb connect IP
+echo.
+echo.
+cmd
